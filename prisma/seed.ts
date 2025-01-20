fa9fb9a80fdfabfc25a8837efebd9e6dd8ac5fda
AhmedMekkawy27 <mekkawy39@gmail.com>
2025-01-20 23:49:00 +0200

    seed fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 import {faker} from '@faker-js/faker';
 const prisma = new PrismaClient()
-async function main() {
-    const newTodo = await prisma.todo.createMany({
-        data: Array.from({length: 25}, ()=> (
-            {
-                title: faker.lorem.words(3),
-                body: faker.lorem.text(),
-            }
-        ))
-    })
-}
+// async function main() {
+//     const newTodo = await prisma.todo.createMany({
+//         data: Array.from({length: 25}, ()=> (
+//             {
+//                 title: faker.lorem.words(3),
+//                 body: faker.lorem.text(),
+//             }
+//         ))
+//     })
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
